2ce98405594c83c05e1ebadf4c5577ddc8f6f6f4
Michael Joyner <joyner.michael@gmail.com>
2017-09-21 10:51:44 +0800

    add activeclass blade directive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::directive('activeclass', function ($path_fragment) {
+        return "<?php echo starts_with(Request::path(), $path_fragment) ? 'active' : ''; ?>";
     }
 
     /**
